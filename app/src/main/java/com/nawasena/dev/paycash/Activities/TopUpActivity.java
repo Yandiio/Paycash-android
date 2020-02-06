abad8e44e12551ee86c9f60e091682d000de1b94
yandiio <yandifenanda95@gmail.com>
2020-02-06 09:41:43 +0700

    Payment added

---
+++ app/src/main/java/com/nawasena/dev/paycash/Activities/TopUpActivity.java
@@ -0,0 +1,16 @@
+package com.nawasena.dev.paycash.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nawasena.dev.paycash.R;
+
+public class TopUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_up);
+    }
+}
